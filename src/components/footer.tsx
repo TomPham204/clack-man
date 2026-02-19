bfab80d21241015482a632803c94e3c176a1cd10
TomPham204 <thect04@gmail.com>
2026-02-19 16:32:09 +0700

    chore: remove partnership

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,7 @@ export default function Footer() {
    return (
 			<div className='max-w-6xl w-full mx-auto border-t border-t-gray-300 px-2 py-3 drop-shadow-md'>
 				<div className='flex flex-col gap-1'>
-					<p>© 2025 Tom Pham. All rights reserved.</p>
-					<p>Created in partnership with TraDaViaHe.</p>
+					<p>© 2026 Tom Pham. All rights reserved.</p>
 				</div>
 			</div>
 		);
